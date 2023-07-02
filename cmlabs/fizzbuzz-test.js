00b50a412ee04b855d194c37d01bc48c1e73e0ce
IyanTRICKSTER <akiyan2002@gmail.com>
2023-07-02 17:33:45 +0700

    Fix initial commit

--- cmlabs/fizzbuzz-test.js
+++ cmlabs/fizzbuzz-test.js
@@ -1,10 +1,10 @@
 for (let i = 1; i <= 100; i++ ){
-    if (i%3 == 0 && i%5==0) {
+    if (i%3 == 0 && i%5==0) { //jika i habis dibagi 3 dan 5 maka kita cetak "FizzBuzz"
         console.log("FizzBuzz")
-    } else if (i%3 == 0) {
+    } else if (i%3 == 0) { //jika i habis dibagi 3 maka cetak "Fizz"
         console.log("Fizz")
-    }  else if (i%5 == 0) {
-        console.log("Fizz")
+    }  else if (i%5 == 0) { //jika i habis dibagi 5 maka cetak "Buzz"
+        console.log("Buzz")
     } else {
         console.log(i)
     }
